08e061295558e9bba231fee867f44bd162866fd3
gillohner <gil@lohner.net>
2025-11-24 03:12:39 +0100

    init event layout

---
+++ app/event/[authorId]/[eventId]/page.tsx
@@ -0,0 +1,14 @@
+import { EventPageLayout } from "@/components/event/event-page-layout";
+
+interface EventPageProps {
+  params: {
+    authorId: string;
+    eventId: string;
+  };
+}
+
+export default function EventPage({ params }: EventPageProps) {
+  const { authorId, eventId } = params;
+
+  return <EventPageLayout authorId={authorId} eventId={eventId} />;
+}
