d54219d6e69882444287d8ab6a0f1ef9a0ae58d2
gillohner <gillohner@github.io>
2025-11-24 23:12:07 +0100

    minimal event pages and sidebar

---
+++ app/event/create/page.tsx
@@ -0,0 +1,5 @@
+import { CreateEventPageLayout } from "@/components/event/create/create-event-page-layout";
+
+export default function CreateEventPage() {
+  return <CreateEventPageLayout mode="create" />;
+}
