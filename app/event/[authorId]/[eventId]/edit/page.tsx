9942e602430e1893564717eab5f49f99fcee8305
gillohner <gil@lohner.net>
2025-12-03 20:07:43 +0100

    event edit (nexus fetching not updating yet)

---
+++ app/event/[authorId]/[eventId]/edit/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { use } from "react";
+import { CreateEventPageLayout } from "@/components/event/create/create-event-page-layout";
+
+interface EditEventPageProps {
+  params: Promise<{
+    authorId: string;
+    eventId: string;
+  }>;
+}
+
+export default function EditEventPage({ params }: EditEventPageProps) {
+  const { authorId, eventId } = use(params);
+
+  return (
+    <CreateEventPageLayout
+      mode="edit"
+      authorId={authorId}
+      eventId={eventId}
+    />
+  );
+}
